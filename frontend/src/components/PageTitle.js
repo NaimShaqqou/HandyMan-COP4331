ddc658d2fea27ca6b12b6fbeaf05befb25bba310
Asir Alam <Alvi.Abrar29@gmail.com>
2022-03-10 19:58:07 -0500

    test MERRN

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -3,7 +3,7 @@ import React from 'react';
 function PageTitle()
 {
     return (
-        <h1 id="title">COP 4331 MERN Stack Demo</h1>
+        <h1 id="title">COP 4331 MERRN Stack Demo</h1>
     );
 };
 
