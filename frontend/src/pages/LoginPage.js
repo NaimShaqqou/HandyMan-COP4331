a3d3b2a5de459f9ed28cff6d230b852ef5042b48
AsirAAlam <46213673+AsirAAlam@users.noreply.github.com>
2022-03-10 20:11:23 -0500

    Add Register Component

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageTitle from '../components/PageTitle';
 import Login from '../components/Login';
+import Register from '../components/Register';
 
 const LoginPage = () =>
 {
@@ -9,6 +10,7 @@ const LoginPage = () =>
         <div>
             <PageTitle />
             <Login />
+            <Register />
         </div>
     );
 };
