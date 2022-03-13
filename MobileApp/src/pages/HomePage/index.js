1c7526979ce6db567505a97064e35330d7a462de
Naim Shaqqou <naeemshaqqou@gmail.com>
2022-03-13 03:57:04 -0400

    add mobile app

---
+++ MobileApp/src/pages/HomePage/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 24, alignSelf: 'center'}}>Welcome to Handler!</Text>
+    </View>
+  )
+}
+
+export default HomePage
