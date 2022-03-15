76ee9c5902dace4defb3508c8216cd01698a72be
abrugal <estebancbrugal@knights.ucf.edu>
2022-03-14 20:22:00 -0400

    Create addService, deleteService and changePassword endpoints. Test endpoints once database is up

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ exports.setApp = function (app, client) {
   var token = require("./createJWT.js");
 
   app.post("/api/addcard", async (req, res, next) => {
-    // incoming: userId, color
+    // incoming: userId, color, jwtToken
     // outgoing: error
 
     const { userId, card, jwtToken } = req.body;
@@ -42,7 +42,7 @@ exports.setApp = function (app, client) {
   });
 
   app.post("/api/searchcards", async (req, res, next) => {
-    // incoming: userId, search
+    // incoming: userId, search, jwtToken
     // outgoing: results[], error
 
     var error = "";
@@ -101,7 +101,7 @@ exports.setApp = function (app, client) {
     var fn = "";
     var ln = "";
     var ret;
-    
+
     if (results.length > 0) {
       id = results[0].UserId;
       fn = results[0].FirstName;
@@ -113,8 +113,178 @@ exports.setApp = function (app, client) {
         ret = { error: e.message };
       }
     } else {
-      ret = { error: "Login/Password incorrect", id: id};
+      ret = { error: "Login/Password incorrect", id: id };
     }
     res.status(200).json(ret);
+  });
+
+  app.post("/api/add-service", async (req, res, next) => {
+    // incoming: userId, title, longitude, latitude, description, price, daysAvailable, category, jwtToken
+    // outgoing: serviceId, error (optional), jwtToken
+    var response;
+
+    const {
+      userId,
+      title,
+      longitude,
+      latitude,
+      description,
+      price,
+      daysAvailable,
+      category,
+      jwtToken,
+    } = req.body;
+
+    try {
+      if (token.isExpired(jwtToken)) {
+        var r = { error: "The JWT is no longer valid", jwtToken: "" };
+        res.status(200).json(r);
+        return;
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    var refreshedToken = null;
+    try {
+      refreshedToken = token.refresh(jwtToken);
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    const db = client.db();
+    const writeResult = await db.collection("Services").insertOne(
+      {
+        UserId: userId,
+        Title: title,
+        Longitude: longitude,
+        Latitude: latitude,
+        Description: description,
+        Price: price,
+        DaysAvailable: daysAvailable,
+        Category: category,
+      },
+      function (err, objectInserted) {
+        if (err) {
+          response = {
+            serviceId: -1,
+            error: err,
+            refreshedToken: refreshedToken,
+          };
+        } else {
+          response = {
+            serviceId: objectInserted[0]._id,
+            refreshedToken: refreshedToken,
+          };
+        }
+      }
+    );
+
+    res.status(200).json(response);
+  });
+
+  app.post("api/delete-service", async (req, res, next) => {
+    // incoming: userId, title, jwtToken
+    // outgoing: error (optional), jwtToken
+
+    const { userId, title, jwtToken } = req.body;
+
+    var response;
+
+    try {
+      if (token.isExpired(jwtToken)) {
+        var r = { error: "The JWT is no longer valid", jwtToken: "" };
+        res.status(200).json(r);
+        return;
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    var refreshedToken = null;
+    try {
+      refreshedToken = token.refresh(jwtToken);
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    const db = client.db();
+    const deleteResult = db
+      .collection("Services")
+      .deleteOne({ UserId: userId, Title: title }, function (err, result) {
+        if (err) {
+          response = {
+            error: err,
+            refreshedToken: refreshedToken,
+          };
+        } else {
+          response = {
+            refreshedToken: refreshedToken,
+          };
+        }
+      });
+
+    res.status(200).json(response);
+  });
+
+  app.post("api/change-password", async (req, res, next) => {
+    // incoming: userId, oldPassword, newPassword, jwtToken
+    // outgoing: error (optional), jwtToken
+
+    const { userId, oldPassword, newPassword, jwtToken } = req.body;
+
+    try {
+      if (token.isExpired(jwtToken)) {
+        var r = { error: "The JWT is no longer valid", jwtToken: "" };
+        res.status(200).json(r);
+        return;
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    var refreshedToken = null;
+    try {
+      refreshedToken = token.refresh(jwtToken);
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    const db = client.db();
+    const user = db
+      .collection("Users")
+      .findOne({ _id: userId, Password: oldPassword });
+
+    if (!user) {
+      var r = {
+        error: "Old password is not correct",
+        jwtToken: refreshedToken,
+      };
+      res.status(200).json(r);
+      return;
+    }
+
+    if (oldPassword == newPassword) {
+      var r = {
+        error: "Passwords can't be the same",
+        jwtToken: refreshedToken,
+      };
+      res.status(200).json(r);
+      return;
+    }
+
+    db.collection("Users").update(
+      { user },
+      { $set: { Password: oldPassword } },
+      function (err, objectReturned) {
+        if (err) {
+          response = { error: err, refreshedToken: refreshedToken };
+        } else {
+          response = { refreshedToken: refreshedToken };
+        }
+      }
+    );
+
+    res.status(200).json(response);
   });
 };
