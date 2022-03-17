bfb11c5b4f45ada13b84addfa2f3353e30c8a342
AsirAAlam <46213673+AsirAAlam@users.noreply.github.com>
2022-03-16 22:29:32 -0400

    Update TestPage.js

--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { NativeBaseProvider, Text, Box } from 'native-base';
 
 const TestPage = () =>
 {
   return (
-    <NativeBaseProvider>
-      <Box flex={1} bg="#aff" alignItems="center" justifyContent="center">
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box>
-    </NativeBaseProvider>
+    <div>
+      <h1>Test Potato ( ͡° ͜ʖ ͡°)</h1>
+    </div>
   );
 };
 
