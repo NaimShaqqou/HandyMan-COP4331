c93fdecc7cfe03882f70f6547427b18e82d0d6cb
Naim Shaqqou <naeemshaqqou@gmail.com>
2022-03-21 17:33:39 -0400

    Added map to homepage

--- HandyManMobile/src/screens/Home.js
+++ HandyManMobile/src/screens/Home.js
@@ -1,6 +1,9 @@
 import { Center, Box } from 'native-base'
 import React from 'react'
+import { Dimensions, StyleSheet } from 'react-native';
+
 import Header from '../components/Header.js'
+import MapView from 'react-native-maps';
 
 const Home = () => {
   return (
@@ -9,8 +12,18 @@ const Home = () => {
       <Box w="100%">
         <Header title="Home" />
       </Box>
+      <Center>
+        <MapView style={styles.map} />
+      </Center>
     </Center>
   )
 }
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
 
 export default Home
