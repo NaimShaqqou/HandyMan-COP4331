3ac6a8c47bf14a69837f7f34e2332d3c238e95f6
Zhichen Xu <zxu0416@knights.ucf.edu>
2022-03-24 01:56:02 -0400

    tried to put the search icon on the right side of the search box, and used two other ways, still needed to be fixed

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -8,8 +8,57 @@ import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Autocomplete from '@mui/material/Autocomplete';
 import ResponsiveAppBar from '../components/NavBar';
-
+import {
+  styled,
+  alpha,
+  InputBase,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  CssBaseline
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search"
 // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto"
+  }
+}));
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+}));
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch"
+      }
+    }
+  }
+}));
 const top100Films = [
   { title: 'The Shawshank Redemption', year: 1994 },
   { title: 'The Godfather', year: 1972 },
@@ -144,14 +193,22 @@ const HomePage = () =>
       <ResponsiveAppBar />
       <h1>Handler</h1>
 
-      <Box sx={classes.box}>
-        <Autocomplete
-          freeSolo
-          id="free-solo-demo"
-          options={top100Films.map((option) => option.title)}
-          renderInput={(params) => <TextField {...params} label="Search Services" />}
-        />
-      </Box>
+     
+
+
+          <Box sx={classes.box}>
+            <Autocomplete
+            freeSolo
+            id="free-solo-demo"
+            options={top100Films.map((option) => option.title)}
+            renderInput={(params) => <TextField {...params} label="Search Services" variant="outlined"
+            />
+          }
+          
+            />
+            <SearchIcon style={{ cursor:"pointer", display:"inline", padding:"17px"}}/>
+           </Box>
+            
     </div>
   );
 }
