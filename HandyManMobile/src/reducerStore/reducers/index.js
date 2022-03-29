a9690ccad3bfaa06f1ce7357c03f592dfb8af17c
Naim Shaqqou <naeemshaqqou@gmail.com>
2022-03-29 13:21:45 -0400

    setup services reducer for mobile

--- HandyManMobile/src/reducerStore/reducers/index.js
+++ HandyManMobile/src/reducerStore/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 
 const reducers = combineReducers({
-    user: userReducer
+    user: userReducer,
+    services: servicesReducer
 });
 
 export default reducers;
---
+++ HandyManMobile/src/reducerStore/reducers/servicesReducer.js
@@ -0,0 +1,35 @@
+// reducer for services owned by logged in user
+
+const reducer = (
+    state = {
+        services: new Array()
+    },
+    action
+) => {
+    switch (action.type) {
+        case "login":
+            return {
+                ...state,
+                services: action.payload.services
+            }
+        case "addService":
+            return {
+                ...state,
+                services: [...state.services, action.payload.service]
+            }
+        case "deleteService":
+            return {
+                ...state,
+                services: state.services.filter(service => service !== action.payload.service)
+            }
+        case "logout":
+            return {
+                ...state,
+                services: new Array()
+            }
+        default:
+            return state
+    }
+}
+
+export default reducer
