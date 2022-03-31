f7ee1906f8a07f9e1d80caf479d1c9871d71a78c
Naim Shaqqou <naeemshaqqou@gmail.com>
2022-03-31 00:12:16 -0400

    my shit is not committing dafuq

--- HandyManMobile/reducerStore/reducers/index.js
+++ HandyManMobile/reducerStore/reducers/index.js
@@ -1,8 +1,11 @@
+// helper file
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
+import servicesReducer from './servicesReducer'
 
 const reducers = combineReducers({
-    user: userReducer
+    user: userReducer,
+    services: servicesReducer
 });
 
 export default reducers;
