113843298401579d4be1b2a91b21afb50fd95677
Zhichen Xu <zxu0416@knights.ucf.edu>
2022-04-05 00:03:56 -0400

    change

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../Title.css'
 import { useNavigate } from "react-router-dom";
 function Logo(){
-    const navigate = useNavigate()
+    
     return (
         <div>
             <p className="logo"><a href="/" >Handler</a></p>
