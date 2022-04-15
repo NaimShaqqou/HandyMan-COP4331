39f891896f72cdbf695a846dc9433642f10d5c8e
Naim Shaqqou <naeemshaqqou@gmail.com>
2022-04-15 00:05:02 -0400

    fixed layout for autocomplete searchbox

--- HandyManMobile/src/components/LocationSearchBar.js
+++ HandyManMobile/src/components/LocationSearchBar.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { PLACES_API_KEY } from "@env"
+import { Icon, Input } from 'native-base';
+import { MaterialIcons } from "@native-base/icons";
 
-const GooglePlacesInput = () => {
+const GooglePlacesInput = ({filterIcon}) => {
 
   const ref = useRef();
 
@@ -62,25 +64,35 @@ const GooglePlacesInput = () => {
     <GooglePlacesAutocomplete
       ref={ref}
       placeholder='Search Location'
-      styles={
-        {
-          textInput: {
-          backgroundColor: '#fff',
-          height: "100%",
-          width: "90%",
-          borderRadius: 1,
-          borderColor: "blue",
-          paddingTop: 5,
-          paddingRight: 0,
-          paddingBottom: 5,
-          paddingLeft: 0,
-          fontSize: 17,
-          flex: 0,
-          },
-          textInputContainer: {
-            alignSelf: "center"
-          }
-        }}
+      textInputProps={{
+        InputComp: Input,
+        InputLeftElement: <Icon size="6" ml="2" as={<MaterialIcons name="search" />} Color="muted.400" />,
+        InputRightElement: filterIcon,
+        w: '100%',
+        h: '50',
+        variant: 'filled',
+        size: 'xl',
+        bgColor: 'white',
+      }}
+      // styles={
+      //   {
+      //     textInput: {
+      //       backgroundColor: '#fff',
+      //       height: "100%",
+      //       width: "90%",
+      //       borderRadius: 1,
+      //       borderColor: "blue",
+      //       paddingTop: 5,
+      //       paddingRight: 0,
+      //       paddingBottom: 5,
+      //       paddingLeft: 0,
+      //       fontSize: 17,
+      //       flex: 0,
+      //     },
+      //     textInputContainer: {
+      //       alignSelf: "center"
+      //     }
+      //   }}
 
       // DO WHATEVER YOU WANT WITH THE LOCATION ONCE IT IS SELECTED
       // ↓↓↓↓↓↓↓
--- HandyManMobile/src/screens/Home.js
+++ HandyManMobile/src/screens/Home.js
@@ -1,57 +1,77 @@
-import { Center, Box, Button, Icon, Input } from 'native-base'
-import { MaterialIcons } from "@native-base/icons"
-import React, { useState } from 'react'
-import {StyleSheet, View, Picker } from 'react-native'
+import { Center, Box, Button, Icon, Input } from "native-base";
+import { MaterialIcons } from "@native-base/icons";
+import React, { useState } from "react";
+import { StyleSheet, View, Picker } from "react-native";
 
-import { Modal, RadioButton, Text } from 'react-native-paper';
+import { Modal, RadioButton, Text } from "react-native-paper";
 
-import ServicesMap from '../components/ServicesMap.js'
-import GooglePlacesInput from '../components/LocationSearchBar.js'
+import ServicesMap from "../components/ServicesMap.js";
+import GooglePlacesInput from "../components/LocationSearchBar.js";
 
 const Home = () => {
-
   const [visible, setVisible] = React.useState(false);
 
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
-  const containerStyle = {backgroundColor: 'white', padding: 20};
+  const containerStyle = { backgroundColor: "white", padding: 20 };
 
   const [search, setSearch] = useState("");
   const [category, setCategory] = useState("java");
-  const [maxDist, setMaxDist] = React.useState('5');
+  const [maxDist, setMaxDist] = React.useState("5");
 
   return (
-    <Center flex={1}>
+    <Center display={"flex"} flex={1}>
       <ServicesMap />
-      <Box w="100%" position={'absolute'} safeAreaTop alignSelf='flex-start' top={-40}>
-        <Box style={styles.searchContainer}>
-          <GooglePlacesInput />
-          <Button style={styles.filterButton} onPress={showModal}>
-          <View>
-              <Icon alignSelf="center" size="8" ml="0" as={<MaterialIcons name="filter-list" />} Color="muted.400" />
-          </View>
-          </Button>
-        </Box>
-      </Box>
-      <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
-        <Input 
-          placeholder="I need..."
-          w="80%"
-          h='30%'
-          borderRadius="4"
-          InputLeftElement={<Icon size="5" ml="2" as={<MaterialIcons name="search" />} Color="muted.400" />}
-          variant="filled"
-          fontSize={14}
-          backgroundColor={'white'}
-          onChangeText={(newSearch) => setSearch(newSearch)}
-          defaultValue={search}
+      <Center w="80%" position={"absolute"} safeAreaTop top={-20}>
+        <GooglePlacesInput
+          filterIcon={
+            <Button variant="unstyled" px="2" py="0" onPress={showModal}>
+              <Icon
+                size="6"
+                ml="0"
+                as={<MaterialIcons name="filter-list" />}
+                Color="muted.400"
+              />
+            </Button>
+          }
         />
-        
-        <RadioButton.Group onValueChange={newValue => setMaxDist(newValue)} value={maxDist}>
+      </Center>
+ 
+      <Modal
+        visible={visible}
+        onDismiss={hideModal}
+        contentContainerStyle={styles.modalContainer}
+      >
+        <Center h="10%">
+          <Input
+            placeholder="I need..."
+            w="80%"
+            h="100%"
+            borderRadius="4"
+            InputLeftElement={
+              <Icon
+                size="5"
+                ml="2"
+                as={<MaterialIcons name="search" />}
+                Color="muted.400"
+              />
+            }
+            variant="outline"
+            fontSize={14}
+            backgroundColor={"white"}
+            onChangeText={(newSearch) => setSearch(newSearch)}
+            defaultValue={search}
+          />
+        </Center>
+
+        <RadioButton.Group
+          onValueChange={(newValue) => setMaxDist(newValue)}
+          value={maxDist}
+        >
           <View style={styles.radioButton}>
             <Text>1</Text>
             <RadioButton value="1" />
-          </View>         
+          </View>
           <View style={styles.radioButton}>
             <Text>5</Text>
             <RadioButton value="5" />
@@ -68,41 +88,29 @@ const Home = () => {
 
         <Picker
           selectedValue={category}
-          style={{ height: 50, width: 150 }}
+          // style={{ height: 50, width: 150 }}
           onValueChange={(itemValue, itemIndex) => setCategory(itemValue)}
         >
           <Picker.Item label="Java" value="java" />
           <Picker.Item label="JavaScript" value="js" />
         </Picker>
       </Modal>
+
     </Center>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  searchContainer: {
-    alignSelf: "center",
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    backgroundColor: "white",
-    borderRadius: 15,
-    width: "95%",
-    padding: 5
-  },
-  filterButton: {
-    alignSelf: "flex-start",
-    flex: 0,
-    height: 25,
-    width: 25,
-    marginTop: 10,
-    marginRight: 10,
-    backgroundColor: "white"
-  },
   radioButton: {
     display: "flex",
     flexDirection: "row",
+  },
+  modalContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    alignSelf: 'center',
+    width: '80%'
   }
 });
 
-export default Home
+export default Home;
